9ac1ec89366f042d38b4bcf396bd5f06b2adc045
Jon Morton <jon.morton@gmail.com>
2015-04-28 22:24:56 -0500

    Define and explain actions

---
+++ js/actions.js
@@ -0,0 +1,8 @@
+/**
+ * Actions are objects that contain (what I call) a signal and payload.
+ *
+ * I don't like calling these events, because although they are similar
+ * to DOM events, they are different. Instead of having to differentiate
+ * between a DOM event vs. a Flux event, I'll just use a different term.
+ *
+ */
