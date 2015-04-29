e6cea1b75fd22d4c2bb7fa7081f57d1c126d23cb
Jon Morton <jon.morton@gmail.com>
2015-04-28 22:18:10 -0500

    Begin structuring project

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("At least this works.")
+/**
+ * Data flow in Flux goes like so:
+ *
+ * [actions] -> [dispatcher] -> [stores] -> [views]
+ *
+ * Because this is a simple project, there is a single file containing
+ * the code for each module (as opposed to a bunch of directories).
+ *
+ */
