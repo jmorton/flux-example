53fe8f1e612fcd11b6a658a546f457493e032b01
Jon Morton <jon.morton@gmail.com>
2015-04-28 22:28:27 -0500

    Define and explain stores

---
+++ js/stores.js
@@ -0,0 +1,15 @@
+/**
+ * Stores manage application state and trigger updates to the view.
+ *
+ * However, stores do not directly reference views.  Instead, they
+ * provide a way to register callback functions that isolate that
+ * aspect elsewhere.
+ *
+ * We rely on two other modules to implement our stores:
+ * 1. EventEmitter
+ * 2. object-assign
+ *
+ */
+
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
